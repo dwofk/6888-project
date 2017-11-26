6fcfadb89a6729719f782c071ce1be5b3abf8c02
Madeleine Waller <mgwaller@mit.edu>
2017-11-26 11:52:41 -0500

    Winograd reference conv - Wofk & Waller

--- models/ws_2d/stimulus.py
+++ models/ws_2d/stimulus.py
@@ -12,8 +12,61 @@ def conv(x, W, b):
             W_c = W[:, :, in_channel, out_channel]
             x_c = x[:, :, in_channel]
             y[:, :, out_channel] += correlate2d(x_c, W_c, mode="same")
+            #y[:, :, out_channel] += correlate2d(x_c, W_c, mode="valid")
         y[:, :, out_channel] += b[out_channel]
     return y
+
+def conv_winograd(x, W, b):
+    print(x.shape, W.shape, b.shape)
+    x = x.astype(np.float64)
+    W = W.astype(np.float64)
+    b = b.astype(np.float64)
+    y = np.zeros([x.shape[0]-W.shape[0]+1, x.shape[1]-W.shape[1]+1, W.shape[3]]).astype(np.float64)
+    for k in range(W.shape[3]):
+        y[:, :, k] += b[k] # apply biases
+        
+    # Winograd transforms
+    B_T = np.array([ [1,0,-1,0],
+                     [0,1,1,0],
+                     [0,-1,1,0],
+                     [0,1,0,-1] ])
+    B = B_T.transpose()
+    G = np.array([ [1,0,0],
+                   [0.5,0.5,0.5],
+                   [0.5,-0.5,0.5],
+                   [0,0,1] ])
+    G_T = G.transpose()
+    A_T = np.array([ [1,1,1,0],
+                     [0,1,-1,-1] ])
+    A = A_T.transpose()
+    
+    # Perform Winograd transforms
+    K = W.shape[3]
+    C = W.shape[2]
+    P = 1 # TODO start with 1 tile for now
+    U = np.zeros([4,4,C,K]) # 4,4,4,8
+    V = np.zeros([4,4,C,P]) # 4,4,4,1
+    M = np.zeros([4,4,K,P])
+    for k in range(K): # filter
+        for c in range(C): # channel
+            g = W[:, :, c, k]# 3x3 filter
+            U[:,:,c,k] = np.dot(G,np.dot(g,G_T)) # 4x4
+    for p in range(P): # input tile
+        for c in range(C): # channel
+            d = x[:,:,c] # 4x4 ifmap tile
+            V[:,:,c,p] = np.dot(B_T,np.dot(d,B)) 
+            
+    # Perform element wise matrix multiplication w/ summation over input channels
+    for p in range(P):
+        for k in range(K):
+            for c in range(C): # sum over input channels C
+                M[:,:,k,p] += np.multiply(U[:,:,c,k],V[:,:,c,p])
+                
+    # Revert Winograd transforms
+    for k in range(K):
+        for p in range(P):
+            y[:,:,k] += np.dot(A_T,np.dot(M[:,:,k,p],A))
+    return y.astype(np.int64)
 
 class Stimulus(Module):
     def instantiate(self, arr_x, arr_y, chn_per_word, input_chn, output_chn):
@@ -53,7 +106,9 @@ class Stimulus(Module):
 
         # Reference Output
         reference = conv(ifmap, weights, bias)
+        reference_winograd = conv_winograd(ifmap, weights, bias)
         print("reference: ", reference)
+        print("winograd reference: ", reference_winograd)
 
         self.serializer.configure(ifmap, weights, bias, image_size, filter_size)
         self.deserializer.configure(ofmap, reference, image_size)
